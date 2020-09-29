6b4c8ec2304aabeb07f4a682a5295ff35a464619
Violetta <vita2424@mail.ru>
2020-09-29 17:26:34 +0300

    Done

---
+++ Lesson2.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+public class Lesson2{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "LVL2 = > LVL3");
+        JOptionPane.showMessageDialog(null,"UP");
+    }
+}
