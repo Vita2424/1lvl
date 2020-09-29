920b873f4eda5e9cffe5edad8fdf428505c3e526
Violetta <vita2424@mail.ru>
2020-09-29 17:44:30 +0300

    Done

---
+++ Lesson3.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Lesson3 {
+    public static void main(String[] args) {
+        String Yname;
+        Yname = JOptionPane.showInputDialog("ИМЯ?");
+        JOptionPane.showMessageDialog(null, "Приятно,\n" + Yname+ "!");
+    }
+}
+
